45204a477cb5fd2f4da5c5fb5f9d9eae939bca5c
Studenti <student@tfzr.rs>
2025-11-27 18:05:42 +0100

    Update main.c

--- projekat/main.c
+++ projekat/main.c
@@ -7,6 +7,17 @@ typedef struct Lista
    struct Lista *sledeci;
 } Lista;
 
+Lista *napraviCvor()
+{
+    Lista *novi = malloc(sizeof(Lista));
+    Lista->sledeci = NULL;
+
+    printf("Uneti vrednost: ");
+    scanf("%d", &novi->vrednost);
+
+    return novi;
+}
+
 int main()
 {
     printf("Hello world!\n");
